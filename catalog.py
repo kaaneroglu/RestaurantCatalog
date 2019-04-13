691196c2b3367990f5bacbad4a81b7eb6ff3d29e
Kaan <kaan@kaan.ca>
2019-04-13 03:31:05 +0000

    Added templates with CRUD functionality

--- catalog.py
+++ catalog.py
@@ -6,26 +6,32 @@ from db_setup import Base, Cuisine, Restaurant
 app = Flask(__name__)
 
 
-engine = create_engine('sqlite:///yycrestaurants.db')
+engine = create_engine('sqlite:///yycrestaurants.db', connect_args={'check_same_thread': False}, echo=True)
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 # Route for READ all cuisines
 @app.route('/')
 @app.route('/cuisines', methods=['GET'])
 def showCuisines():
-    cuisines= session.query(Cuisine).all()
+    cuisines = session.query(Cuisine).all()
     return render_template('index.html', title='Restaurant Catalog', cuisines=cuisines)
 
 # Route for CREATE new cuisine
 @app.route('/cuisines/new/', methods=['GET', 'POST'])
 def newCuisine():
     if request.method == 'POST':
-        return "Create new cuisine"
+        newCuisineName = Cuisine(name=request.form['name'])
+        session.add(newCuisineName)
+        session.commit()
+        flash("New cuisine created!")
+        return redirect(url_for('showCuisines'))
     else:
-        return "This page will have a form to take input cuisine name and create that"
+        cuisines = session.query(Cuisine).all()
+        return render_template('createcuisine.html', title='Restaurant Catalog', cuisines=cuisines)
 
 # Route for UPDATE cuisine
 @app.route('/cuisines/<int:cuisine_id>/edit/', methods=['GET', 'POST'])
@@ -47,11 +53,12 @@ def deleteCuisine(cuisine_id):
 @app.route('/cuisines/<int:cuisine_id>/', methods=['GET'])
 @app.route('/cuisines/<int:cuisine_id>/restaurant/', methods=['GET'])
 def showRestaurants(cuisine_id):
-    return "This page shows all restaurants for cuisine %s" % cuisine_id
+    cuisines = session.query(Cuisine).all()
+    cuisineName  = session.query(Cuisine).filter_by(id=cuisine_id).one()
+    restaurants = session.query(Restaurant).filter_by(cuisine_id=cuisine_id)
+    return render_template('restaurants.html', title='Restaurant Catalog', cuisines=cuisines, restaurants=restaurants, cuisineName=cuisineName)
 
 # Route for CREATE new restaurant
-
-
 @app.route(
     '/cuisines/<int:cuisine_id>/restaurant/new/',
     methods=[
@@ -59,13 +66,17 @@ def showRestaurants(cuisine_id):
         'POST'])
 def newRestaurant(cuisine_id):
     if request.method == 'POST':
-        return "Create new restaurant"
+        newRestaurantInfo = Restaurant(name = request.form['name'], description = request.form['description'], phone = request.form['phone'], website = request.form['website'], address = request.form['address'], cuisine_id = cuisine_id)
+        session.add(newRestaurantInfo)
+        session.commit()
+        flash("New restaurant created!")
+        return redirect(url_for('showRestaurants',cuisine_id = cuisine_id))
     else:
-        return "Create new restaurant under cuisine %s" % cuisine_id
+        cuisines = session.query(Cuisine).all()
+        cuisineName = session.query(Cuisine).filter_by(id=cuisine_id).one()
+        return render_template('createrestaurant.html', cuisine_id = cuisine_id, title='Restaurant Catalog', cuisines=cuisines, cuisineName=cuisineName)
 
 # Route for UPDATE restaurant
-
-
 @app.route(
     '/cuisines/<int:cuisine_id>/restaurant/<int:restaurant_id>/edit/',
     methods=[
@@ -80,8 +91,6 @@ def editRestaurant(cuisine_id, restaurant_id):
             restaurant_id, cuisine_id)
 
 # Route for DELETE restaurant
-
-
 @app.route(
     '/cuisines/<int:cuisine_id>/restaurant/<int:restaurant_id>/delete/',
     methods=[
@@ -108,6 +117,6 @@ def restaurantDetaisJSON(cuisine_id, restaurant_id):
 
 
 if __name__ == '__main__':
-    # app.secret_key = 'super_Secret_key' #create sessions
+    app.secret_key = 'super_Secret_key' #create sessions
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
